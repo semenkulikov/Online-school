e2934f3cb56a2928b31174651bd4ab11b090e437
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-04-30 23:09:59 +0300

    Fixed health endpoint

--- core/views.py
+++ core/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('<h1>Service started successfully!</h1>')
+    return HttpResponse('<h1>Service started successfully!</h1><h2>Сервис запущен успешно!</h2>')
