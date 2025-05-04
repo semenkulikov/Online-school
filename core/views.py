6caab1509a83a4bad88d55e8b2e9c1296ec2fed8
Semen Saifutdinov <saifutdinov.semen@mail.ru>
2025-05-04 07:03:51 +0300

    Added docs to test view

--- core/views.py
+++ core/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
+    """ Тестовый эндпоинт для проверки работоспособности сервиса """
     return HttpResponse('<h1>Service started successfully!</h1><h2>Сервис запущен успешно!</h2>')
